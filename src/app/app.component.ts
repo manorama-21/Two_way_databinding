2745b1f36e1d21213908cee478e468643b84ed9d
MANORAMA <manorama210688@gmail.com>
2024-02-21 16:45:05 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'two_w_databinding';
+  uname="";
+  but(val:any){
+    this.uname=val
+  }
+}
